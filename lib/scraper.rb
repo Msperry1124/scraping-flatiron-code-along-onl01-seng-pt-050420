537f5da243502f3689d0926cb9a814aa0d2d1505
Melissa Sperry <sperry.mel@gmail.com>
2020-06-09 17:10:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,16 +5,7 @@ require 'pry'
 require_relative './course.rb'
 
 class Scraper
+  def get_page
 
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
   end
-
 end
